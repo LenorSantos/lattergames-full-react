cd881bb5db876469794bedef8090ef69f4ec1655
Fabricio Monteiro Dos Santos <105099322+LenorSantos@users.noreply.github.com>
2022-09-08 10:44:37 -0300

    Update reqserv.js

--- src/js/reqserv.js
+++ src/js/reqserv.js
@@ -1,19 +1,19 @@
 import axios from "axios";
 
 export function dadostop(dados) {
-  axios.get("http://192.168.1.77:3001/pulltoppage").then((result) => {
+  axios.get("http://localhost:3001/pulltoppage").then((result) => {
     dados(result.data);
   });
 }
 
 export function dadospromo(dados) {
-  axios.get("http://192.168.1.77:3001/pullpromo").then((result) => {
+  axios.get("http://localhost:3001/pullpromo").then((result) => {
     dados(result.data);
   });
 }
 
 export function dadosdestaques(dados) {
-  axios.get("http://192.168.1.77:3001/pulldestaques").then((result) => {
+  axios.get("http://localhost:3001/pulldestaques").then((result) => {
     dados(result.data);
   });
 }
